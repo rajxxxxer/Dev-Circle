f3a52443d466fb215a8ebc6253ab0d12fc58f6af
Abhishek Raj <abhishekraj2447@gmail.com>
2025-11-11 22:36:36 +0530

    design post card

--- src/components/Postcard.jsx
+++ src/components/Postcard.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { BadgeCheck } from'lucide-react'
+import moment from 'moment'
 
 const Postcard = ({post}) => {
   return (
-    <div>Postcard</div>
+    <div className='bg-white rounded-cl w-full p-4 space-y-4 max-w-2xl'>
+      
+    <img src={post.user.profile_picture} alt="" />
+    <div>
+      <div className='flex items-center space-x-1'>
+        <span>
+          {post.user.full_name}
+          <BadgeCheck></BadgeCheck>
+        </span>
+      </div>
+      <div>@{post.user.username}dot {moment(post.createdAt).fromNow()}</div>
+    </div>
+    <div> {post.content&& <p>{post.content}</p> }</div>
+
+    <div>
+      
+    </div>
+
+ 
+    </div>
   )
 }
 
