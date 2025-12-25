8ba93e8ca8cbfeacfdebf6cc4f90405303376d75
Abhishek Raj <abhishekraj2447@gmail.com>
2025-12-25 18:22:07 +0530

    completed descover

--- src/components/Usercard.jsx
+++ src/components/Usercard.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { dummyUserData } from '../assets/assets'
+import { MapPin, MessageCircle, Plus, UserPlus } from 'lucide-react'
 
-const Usercard = () => {
-  const user=dummyUserData
+const Usercard = ({user}) => {
+  const curruser=dummyUserData
   const handlefollow= async ()=>{
 
   }
@@ -18,6 +19,26 @@ const Usercard = () => {
       {user.username && <p>@{user.username}</p>}
       {user.bio && <p>{user.bio}</p>}
       
+      </div>
+
+      <div>
+        <div>
+          <MapPin className='w-4 h-4'>{user.location}</MapPin>
+        </div>
+        <div>
+          <span>{user.followers.length}  followers</span>
+        </div>
+      </div>
+      <div>
+        <button onClick={handlefollow} disabled={curruser?.following.includes(user._id)}>
+          <UserPlus>{curruser?.following.includes(user._id) ? "Following" : "Follow"  }</UserPlus>
+        </button>
+
+        <button onClick={handleconnection}>
+          {
+            curruser?.connections.includes(user._id) ? <MessageCircle className='w-4 h-4' /> : <Plus className='w-4 h-4' />
+          }
+        </button>
       </div>
     </div>
   )
